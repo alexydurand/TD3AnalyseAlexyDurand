9c64da9eae064519c417982a1d1610a93175b114
alexydurand <alexy.drnd25@gmail.com>
2025-09-18 12:04:23 +0200

    methode hello() et test unitaire

---
+++ hello-world/src/test/java/com/iutblagnac/AppTest.java
@@ -0,0 +1,12 @@
+package com.iutblagnac;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testHello() {
+        assertEquals("Hello World!", App.hello());
+    }
+}
